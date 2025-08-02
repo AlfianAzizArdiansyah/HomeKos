019cf1c20f1b0a6ee2b94a9074bd499baf96a497
Raihan-rais07 <2200018225@webmail.uad.ac.id>
2025-08-02 15:03:23 +0700

    simpan perubahan terbaru sebelum pull

--- resources/views/penghuni/pembayaran/transfer.blade.php
+++ resources/views/penghuni/pembayaran/transfer.blade.php
@@ -91,8 +91,8 @@
 
         <div class="flex justify-center">
             <a href="{{ route('penghuni.dashboard') }}"
-                class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-8 rounded-lg shadow-lg transition duration-200 whitespace-nowrap text-center">
-                Kembali ke Dashboard
+                class="w-40 bg-blue-600 hover:bg-blue-700 text-white text-lg font-medium py-3 rounded-xl shadow-md text-center">
+                Kembali
             </a>
         </div>
     </div>
